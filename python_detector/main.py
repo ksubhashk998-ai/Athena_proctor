850f403eb6c6523a385a3c2ed9d0bb0b0126eee6
Subhash k <ksubhashk998@gmail.com>
2026-09-17 19:11:38 +0530

    test render auto deploys

--- python_detector/main.py
+++ python_detector/main.py
@@ -808,4 +808,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8001))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-    # Render auto deploy test
+    
